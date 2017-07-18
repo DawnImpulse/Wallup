ded38d009ffefce38071f05ccd1152fc1b6a9301
Saksham <saksham@stonevire.com>
2017-07-18 22:01:17 +0530

    Test : Test 2 Live Images Signed-off-by: Saksham <saksham@stonevire.com>

--- app/src/main/java/com/stonevire/wallup/services/LiveImagesService.java
+++ app/src/main/java/com/stonevire/wallup/services/LiveImagesService.java
@@ -1,7 +1,9 @@
 package com.stonevire.wallup.services;
 
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Point;
 import android.net.Uri;
@@ -22,8 +24,15 @@ import com.facebook.imagepipeline.image.CloseableBitmap;
 import com.facebook.imagepipeline.image.CloseableImage;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.pixplicity.easyprefs.library.Prefs;
+import com.stonevire.wallup.utils.Const;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * Created by Saksham on 7/18/2017.
@@ -32,15 +41,24 @@ import java.util.ArrayList;
 public class LiveImagesService extends WallpaperService {
     @Override
     public Engine onCreateEngine() {
-        Fresco.initialize(this);
+        if (!Fresco.hasBeenInitialized())
+            Fresco.initialize(this);
+        new Prefs.Builder()
+                .setContext(this)
+                .setMode(ContextWrapper.MODE_PRIVATE)
+                .setPrefsName(getPackageName())
+                .setUseDefaultSharedPreference(true)
+                .build();
         return new LiveImagesServiceEngine();
     }
 
     private class LiveImagesServiceEngine extends Engine {
-        ArrayList<Bitmap> bitmapList;
         private Handler handler = new Handler();
+        File mFile;
+
         private boolean visible = true;
-        int i = 0;
+        int position;
+        int cachedSize = 5;
 
         WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
         Point point = new Point();
@@ -49,22 +67,113 @@ public class LiveImagesService extends WallpaperService {
         private final Runnable drawRunner = new Runnable() {
             @Override
             public void run() {
-                draw();
-                handler.postDelayed(drawRunner, 20*1000);
+                if (directory().listFiles().length != 0) {
+                    position = Prefs.getInt(Const.LIVE_IMAGES_POSITION, 0);
+                    Log.d("Test", String.valueOf(position));
+                    if (position > directory().listFiles().length)
+                        position = 0;
+                    if (position == directory().listFiles().length - 1 || position == directory().listFiles().length - 2) {
+                        fetchImage(false);
+                        draw();
+                    } else
+                        draw();
+                } else {
+                    fetchImage(true);
+                    fetchImage(false);
+                    fetchImage(false);
+                    fetchImage(false);
+                    fetchImage(false);
+                }
+                handler.postDelayed(drawRunner, 10 * 1000);
+
             }
 
         };
 
         public LiveImagesServiceEngine() {
+            cachedSize = Prefs.getInt(Const.LIVE_IMAGES_CACHE_SIZE, 5);
+            mFile = directory();
+            handler.post(drawRunner);
+        }
+
+        @Override
+        public void onSurfaceRedrawNeeded(SurfaceHolder holder) {
+            super.onSurfaceRedrawNeeded(holder);
+            handler.post(drawRunner);
+        }
+
+        @Override
+        public void onSurfaceCreated(SurfaceHolder holder) {
+            super.onSurfaceCreated(holder);
+            handler.post(drawRunner);
+        }
+
+        @Override
+        public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            super.onSurfaceChanged(holder, format, width, height);
+            handler.post(drawRunner);
+        }
+
+        @Override
+        public void onSurfaceDestroyed(SurfaceHolder holder) {
+            super.onSurfaceDestroyed(holder);
+            handler.removeCallbacks(drawRunner);
+        }
+
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            super.onVisibilityChanged(visible);
+            if (visible) {
+                handler.post(drawRunner);
+            } else {
+                handler.removeCallbacks(drawRunner);
+            }
+        }
+
+        private void draw() {
+            final SurfaceHolder holder = getSurfaceHolder();
+            Canvas c = null;
+            try {
+                c = holder.lockCanvas();
+                if (c != null) {
+                    mFile = directory();
+                    if (mFile.length() != 0) {
+                        File[] files = mFile.listFiles();
+                        Arrays.sort(files, new Comparator<File>() {
+                            public int compare(File f1, File f2) {
+                                return Long.compare(f1.lastModified(), f2.lastModified());
+                            }
+                        });
+
+                        c.drawBitmap(getFromInternalStorage(files[position].getName()), 0, 0, null);
+
+                        if (directory().listFiles().length == position + 1)
+                            Prefs.putInt(Const.LIVE_IMAGES_POSITION, 0);
+                        else
+                            Prefs.putInt(Const.LIVE_IMAGES_POSITION, ++position);
+                    } else
+                        fetchImage(true);
+                }else
+                {
+                    Log.d("Test","Canvas is Null !!");
+                }
+            } catch (IllegalStateException e) {
+                Log.d("Test", "Inside Exception");
+                e.printStackTrace();
+            } finally {
+                if (c != null) holder.unlockCanvasAndPost(c);
+            }
+        }
+
+        private void fetchImage(final boolean refresh) {
             ImageRequest request = null;
-            bitmapList = new ArrayList<Bitmap>();
             display.getSize(point);
-            
+
             request = ImageRequestBuilder
                     .newBuilderWithSource(Uri.parse("https://source.unsplash.com/random/1980x1080"))
                     .build();
 
-            ImagePipeline imagePipeline = Fresco.getImagePipeline();
+            final ImagePipeline imagePipeline = Fresco.getImagePipeline();
             imagePipeline.evictFromCache(Uri.parse("https://source.unsplash.com/random/1980x1080"));
             DataSource<CloseableReference<CloseableImage>>
                     dataSource = imagePipeline.fetchDecodedImage(request, this);
@@ -77,36 +186,45 @@ public class LiveImagesService extends WallpaperService {
                     }
                     CloseableReference<CloseableImage> closeableImageRef = dataSource.getResult();
                     if (closeableImageRef != null && closeableImageRef.get() instanceof CloseableBitmap) {
-                        Bitmap bmp = ((CloseableBitmap) closeableImageRef.get()).getUnderlyingBitmap();
+                        try {
+                            Bitmap bmp = ((CloseableBitmap) closeableImageRef.get()).getUnderlyingBitmap();
 
-                        int ow = bmp.getWidth();
-                        int oh = bmp.getHeight();
+                            int ow = bmp.getWidth();
+                            int oh = bmp.getHeight();
 
-                        int a = (oh / 16) * 9;
+                            int a = (oh / 16) * 9;
 
-                        Bitmap b = Bitmap.createBitmap(bmp, ow/2 - oh/2, 0, a, oh);
+                            Bitmap b = Bitmap.createBitmap(bmp, ow / 2 - oh / 2, 0, a, oh);
 
-                        int o1 = b.getWidth();
-                        int o2 = b.getHeight();
+                            int o1 = b.getWidth();
+                            int o2 = b.getHeight();
 
-                        /*final float horizontalScaleFactor = (float) o1 / (float) point.x;
-                        final float verticalScaleFactor = (float) o2 / (float) point.y;
+                            Bitmap r = Bitmap.createScaledBitmap(b, point.x, point.y, true);
+                            saveToInternalStorage(r);
+                            //r.recycle();
+                            b.recycle();
 
-                        Log.d("Test", String.valueOf(horizontalScaleFactor) + " -- " + verticalScaleFactor);
-                        final float scaleFactor = Math.max(verticalScaleFactor, horizontalScaleFactor);
-                        final int finalWidth = (int) (o1 / scaleFactor);
-                        final int finalHeight = (int) (o2 / scaleFactor);*/
+                            if (refresh)
+                                handler.post(drawRunner);
 
-                        /*final Bitmap wallpaperBmp = Bitmap.createScaledBitmap(
-                                bmp, o1, o2, true);
-
-                        bmp.recycle();*/
-
-                        Bitmap r = Bitmap.createScaledBitmap(b,point.x,point.y,true);
-                        b.recycle();
-                        bitmapList.add(r);
-
-                        handler.post(drawRunner);
+                            if (directory().listFiles().length >= cachedSize + 1) ;
+                            {
+                                int j = directory().listFiles().length - cachedSize;
+                                if (position >= j) {
+                                    File[] f = directory().listFiles();
+                                    Arrays.sort(f, new Comparator<File>() {
+                                        public int compare(File f1, File f2) {
+                                            return Long.compare(f1.lastModified(), f2.lastModified());
+                                        }
+                                    });
+                                    for (int i = 0; i < j; i++) {
+                                        f[i].delete();
+                                    }
+                                }
+                            }
+                        } finally {
+                            closeableImageRef.close();
+                        }
                     }
                 }
 
@@ -117,29 +235,53 @@ public class LiveImagesService extends WallpaperService {
             }, UiThreadImmediateExecutorService.getInstance());
         }
 
-        private void draw() {
-            final SurfaceHolder holder = getSurfaceHolder();
-            Canvas c = null;
-
+        private void saveToInternalStorage(Bitmap bitmapImage) {
+            FileOutputStream fos = null;
+            int count = 1;
             try {
-                c = holder.lockCanvas();
-                Log.d("Test", "Inside Try");
-                if (c != null) {
-                    if (i == 0) {
-                        c.drawBitmap(bitmapList.get(0), 0, 0, null);
-                        //i++;
-                    } else {
-                        c.drawBitmap(bitmapList.get(1), 0, 0, null);
-                        i--;
-                    }
-                }
-            } catch (IllegalStateException e) {
-                Log.d("Test", "Inside Exception");
+                count = Prefs.getInt(Const.LIVE_IMAGES_COUNT, 1);
+                fos = new FileOutputStream(directoryFile("wall" + count));
+                Prefs.putInt(Const.LIVE_IMAGES_COUNT, ++count);
+                bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            } catch (Exception e) {
                 e.printStackTrace();
             } finally {
-                if (c != null) holder.unlockCanvasAndPost(c);
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private Bitmap getFromInternalStorage(String fileName) {
+            Bitmap b = null;
+            FileInputStream fos = null;
+            try {
+                fos = new FileInputStream(directoryFile(fileName));
+                b = BitmapFactory.decodeStream(fos);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
+            return b;
+        }
+
+        private File directoryFile(String fileName) {
+            ContextWrapper cw = new ContextWrapper(getApplicationContext());
+            File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+            return new File(directory, fileName);
+        }
+
+        private File directory() {
+            ContextWrapper cw = new ContextWrapper(getApplicationContext());
+            return cw.getDir("imageDir", Context.MODE_PRIVATE);
         }
     }
 }
--- app/src/main/java/com/stonevire/wallup/utils/Const.java
+++ app/src/main/java/com/stonevire/wallup/utils/Const.java
@@ -7,96 +7,101 @@ package com.stonevire.wallup.utils;
 public class Const {
 
     //----------------- API Endpoints ----------------------------
-    private static final String DOMAIN                  = "https://api.stonevire.com/wallup/";
-    public static final String NEW_IMAGES               = DOMAIN + "v1/listNewImages";
-    public static final String TAGGING_IMAGES           = DOMAIN + "v1/taggingList";
+    private static final String DOMAIN = "https://api.stonevire.com/wallup/";
+    public static final String NEW_IMAGES = DOMAIN + "v1/listNewImages";
+    public static final String TAGGING_IMAGES = DOMAIN + "v1/taggingList";
 
-    public static final String UNSPLASH_ID              = "?client_id=e1d86d6d0dcb8ce3eea9d16c712c0feed5f1e9a05e5a5f252cf5ea090a3ebc80";
+    public static final String UNSPLASH_ID = "?client_id=e1d86d6d0dcb8ce3eea9d16c712c0feed5f1e9a05e5a5f252cf5ea090a3ebc80";
     //Dawn ID    - a25247a07df2c569f6f3dc129f43b0eb3b0e3ff69b00d5b84dd031255e55b961
     //Saksham ID - e1d86d6d0dcb8ce3eea9d16c712c0feed5f1e9a05e5a5f252cf5ea090a3ebc80
-    public static final String UNSPLASH_API             = "https://api.unsplash.com/";
-    public static final String UNSPLASH_SOURCE          = "https://source.unsplash.com/";
-    public static final String UNSPLASH_USER_RANDOM     = UNSPLASH_SOURCE + "user/";
-    public static final String UNSPLASH_SEARCH          = UNSPLASH_SOURCE + "search/photos" + UNSPLASH_ID + "&query=";
-    public static final String UNSPLASH_LATEST_IMAGES   = UNSPLASH_API + "photos" + UNSPLASH_ID + "&order_by=latest&per_page=30";
-    public static final String UNSPLASH_CURATED_IMAGES  = UNSPLASH_API + "photos/curated" + UNSPLASH_ID + "&order_by=latest&per_page=30";
-    public static final String UNSPLASH_GET_PHOTO       = UNSPLASH_API + "photos/";
+    public static final String UNSPLASH_API = "https://api.unsplash.com/";
+    public static final String UNSPLASH_SOURCE = "https://source.unsplash.com/";
+    public static final String UNSPLASH_USER_RANDOM = UNSPLASH_SOURCE + "user/";
+    public static final String UNSPLASH_SEARCH = UNSPLASH_SOURCE + "search/photos" + UNSPLASH_ID + "&query=";
+    public static final String UNSPLASH_LATEST_IMAGES = UNSPLASH_API + "photos" + UNSPLASH_ID + "&order_by=latest&per_page=30";
+    public static final String UNSPLASH_CURATED_IMAGES = UNSPLASH_API + "photos/curated" + UNSPLASH_ID + "&order_by=latest&per_page=30";
+    public static final String UNSPLASH_GET_PHOTO = UNSPLASH_API + "photos/";
 
     //----------------- Unsplash Image Properties------------------
 
-    public static final String IMAGE_ID             = "id";
-    public static final String IMAGE_URLS           = "urls";
-    public static final String IMAGE_USER           = "user";
-    public static final String IMAGE_USER_IMAGES    = "profile_image";
-    public static final String IMAGE_COLOR          = "color";
-    public static final String IMAGE_RAW            = "raw";
-    public static final String IMAGE_USER_NAME      = "name";
-    public static final String IMAGE_LIKES          = "likes";
-    public static final String LOCATION_OBJECT      = "location";
-    public static final String LOCATION_TITLE       = "title";
-    public static final String USER_IMAGE_LARGE     = "large";
-    public static final String USER_TOTAL_LIKES     = "total_likes";
-    public static final String USER_TOTAL_PHOTOS    = "total_photos";
-    public static final String USER_FIRST_NAME      = "first_name";
-    public static final String USER_LAST_NAME       = "last_name";
-    public static final String USER_PHOTOS          = "photos";
-    public static final String USER_LOCATION        = "location";
-    public static final String USERNAME             = "username";
-    public static final String LINKS                = "links";
-    public static final String ERRORS               = "errors";
-    public static final String IMAGE_REGULAR        = "regular";
-    public static final String EXIF                 = "exif";
-    public static final String CAMERA_MAKE          = "make";
-    public static final String CAMERA_MODEL         = "model";
+    public static final String IMAGE_ID = "id";
+    public static final String IMAGE_URLS = "urls";
+    public static final String IMAGE_USER = "user";
+    public static final String IMAGE_USER_IMAGES = "profile_image";
+    public static final String IMAGE_COLOR = "color";
+    public static final String IMAGE_RAW = "raw";
+    public static final String IMAGE_USER_NAME = "name";
+    public static final String IMAGE_LIKES = "likes";
+    public static final String LOCATION_OBJECT = "location";
+    public static final String LOCATION_TITLE = "title";
+    public static final String USER_IMAGE_LARGE = "large";
+    public static final String USER_TOTAL_LIKES = "total_likes";
+    public static final String USER_TOTAL_PHOTOS = "total_photos";
+    public static final String USER_FIRST_NAME = "first_name";
+    public static final String USER_LAST_NAME = "last_name";
+    public static final String USER_PHOTOS = "photos";
+    public static final String USER_LOCATION = "location";
+    public static final String USERNAME = "username";
+    public static final String LINKS = "links";
+    public static final String ERRORS = "errors";
+    public static final String IMAGE_REGULAR = "regular";
+    public static final String EXIF = "exif";
+    public static final String CAMERA_MAKE = "make";
+    public static final String CAMERA_MODEL = "model";
     public static final String CAMERA_SHUTTER_SPEED = "exposure_time";
-    public static final String CAMERA_FOCAL_LENGTH  = "focal_length";
-    public static final String CAMERA_APERTURE      = "aperture";
-    public static final String CAMERA_ISO           = "iso";
-    public static final String IMAGE_CREATED        = "created_at";
-    public static final String IMAGE_UPDATED_AT     = "updated_at";
-    public static final String IMAGE_HEIGHT         = "height";
-    public static final String IMAGE_WIDTH          = "width";
+    public static final String CAMERA_FOCAL_LENGTH = "focal_length";
+    public static final String CAMERA_APERTURE = "aperture";
+    public static final String CAMERA_ISO = "iso";
+    public static final String IMAGE_CREATED = "created_at";
+    public static final String IMAGE_UPDATED_AT = "updated_at";
+    public static final String IMAGE_HEIGHT = "height";
+    public static final String IMAGE_WIDTH = "width";
 
     //------------------ API Variables ----------------------------
 
-    public static final String PAGE_NO      = "page_no";
-    public static final String SUCCESS      = "success";
-    public static final String IMAGES       = "images";
-    public static final String ERRORID      = "errorID";
-    public static final String DETAILS      = "details";
-    public static final String TAGS         = "tags";
+    public static final String PAGE_NO = "page_no";
+    public static final String SUCCESS = "success";
+    public static final String IMAGES = "images";
+    public static final String ERRORID = "errorID";
+    public static final String DETAILS = "details";
+    public static final String TAGS = "tags";
 
     //------------------ Callbacks --------------------------------
 
-    public static final int NEW_IMAGES_CALLBACK             = 1;
-    public static final int LOAD_MORE_IMAGES_CALLBACK       = 2;
-    public static final int USER_IMAGES_CALLBACK            = 3;
-    public static final int LATEST_CALLBACK                 = 4;
-    public static final int LATEST_LOAD_MORE                = 5;
-    public static final int CURATED_CALLBACK                = 6;
-    public static final int CURATED_LOAD_MORE               = 7;
-    public static final int IMAGE_PREVIEW_DETAIL_CALLBACK   = 8;
-    public static final int USER_IMAGES_LOADING_CALLBACK    = 9;
+    public static final int NEW_IMAGES_CALLBACK = 1;
+    public static final int LOAD_MORE_IMAGES_CALLBACK = 2;
+    public static final int USER_IMAGES_CALLBACK = 3;
+    public static final int LATEST_CALLBACK = 4;
+    public static final int LATEST_LOAD_MORE = 5;
+    public static final int CURATED_CALLBACK = 6;
+    public static final int CURATED_LOAD_MORE = 7;
+    public static final int IMAGE_PREVIEW_DETAIL_CALLBACK = 8;
+    public static final int USER_IMAGES_LOADING_CALLBACK = 9;
 
     //------------------ Transition Names -------------------------
-    public static final String TRANS_NEW_TO_PROFILE         = "trans1";
-    public static final String TRANS_NEW_TO_PROFILE_1       = "trans2";
-    public static final String TRANS_NEW_TO_PROFILE_2       = "trans3";
-    public static final String TRANS_NEW_TO_PREVIEW_3       = "trans4";
-    public static final String TRANS_LATEST_TO_PROFILE      = "trans1";
-    public static final String TRANS_LATEST_TO_PROFILE_1    = "trans2";
-    public static final String TRANS_LATEST_TO_PROFILE_2    = "trans3";
-    public static final String TRANS_LATEST_TO_PREVIEW      = "trans4";
-    public static final String TRANS_CURATED_TO_PROFILE     = "trans1";
-    public static final String TRANS_CURATED_TO_PROFILE_1   = "trans2";
-    public static final String TRANS_CURATED_TO_PROFILE_2   = "trans3";
-    public static final String TRANS_CURATED_TO_PREVIEW     = "trans4";
-    public static final String TRANS_USER_TO_PREVIEW        = "trans4";
+    public static final String TRANS_NEW_TO_PROFILE = "trans1";
+    public static final String TRANS_NEW_TO_PROFILE_1 = "trans2";
+    public static final String TRANS_NEW_TO_PROFILE_2 = "trans3";
+    public static final String TRANS_NEW_TO_PREVIEW_3 = "trans4";
+    public static final String TRANS_LATEST_TO_PROFILE = "trans1";
+    public static final String TRANS_LATEST_TO_PROFILE_1 = "trans2";
+    public static final String TRANS_LATEST_TO_PROFILE_2 = "trans3";
+    public static final String TRANS_LATEST_TO_PREVIEW = "trans4";
+    public static final String TRANS_CURATED_TO_PROFILE = "trans1";
+    public static final String TRANS_CURATED_TO_PROFILE_1 = "trans2";
+    public static final String TRANS_CURATED_TO_PROFILE_2 = "trans3";
+    public static final String TRANS_CURATED_TO_PREVIEW = "trans4";
+    public static final String TRANS_USER_TO_PREVIEW = "trans4";
+
+    //------------------ Live Images ------------------------------
+
+    public static final String LIVE_IMAGES_POSITION = "position";
+    public static final String LIVE_IMAGES_CACHE_SIZE = "cache_size";
+    public static final String LIVE_IMAGES_COUNT = "count";
     //------------------ Others -----------------------------------
 
     public static final String IMAGE_OBJECT = "imageObject";
     public static final String IS_DIRECT_OBJECT = "isDirectObject";
-
 
 
 }
